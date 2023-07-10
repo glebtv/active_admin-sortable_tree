df64e899b8ee9102d1355c21f60dd8f5471cee6c
Gleb Tv <glebtv@gmail.com>
2023-07-10 14:21:58 +0300

    support webpacker

--- lib/active_admin/sortable_tree/engine.rb
+++ lib/active_admin/sortable_tree/engine.rb
@@ -8,19 +8,23 @@ module ActiveAdmin
       config.aa_sortable_tree = ActiveSupport::OrderedOptions.new
       config.aa_sortable_tree.register_assets = true
 
-      initializer "active_admin-sortable_tree.precompile", group: :all do |app|
-        app.config.assets.precompile += [
-          "active_admin/sortable.css",
-          "active_admin/sortable.js"
-        ]
-      end
-
-      initializer "active_admin-sortable_tree.register_assets" do
-        if config.aa_sortable_tree.register_assets
-          ActiveAdmin.application.register_stylesheet "active_admin/sortable.css"
-          ActiveAdmin.application.register_javascript "active_admin/sortable.js"
+        initializer "active_admin-sortable_tree.precompile", group: :all do |app|
+          if !ActiveAdmin.application.respond_to?(:use_webpacker) || !ActiveAdmin.application.use_webpacker
+            app.config.assets.precompile += [
+              "active_admin/sortable.css",
+              "active_admin/sortable.js"
+            ]
+          end
         end
-      end
+
+        initializer "active_admin-sortable_tree.register_assets" do
+          if !ActiveAdmin.application.respond_to?(:use_webpacker) || !ActiveAdmin.application.use_webpacker
+            if config.aa_sortable_tree.register_assets
+              ActiveAdmin.application.register_stylesheet "active_admin/sortable.css"
+              ActiveAdmin.application.register_javascript "active_admin/sortable.js"
+            end
+          end
+        end
     end
   end
 end
