5f6a882cd788056bc4eed1952ec7d3ed5a320d1b
Gleb Tv <glebtv@gmail.com>
2023-08-21 19:08:30 +0300

    support activeadmin_blaze_theme

--- lib/active_admin/views/index_as_sortable.rb
+++ lib/active_admin/views/index_as_sortable.rb
@@ -150,13 +150,13 @@ module ActiveAdmin
         links = ''.html_safe
         if @default_actions
           if controller.action_methods.include?('show') && authorized?(ActiveAdmin::Auth::READ, resource)
-            links << link_to(I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link")
+            links << link_to(I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link c-button c-button--info u-xsmall")
           end
           if controller.action_methods.include?('edit') && authorized?(ActiveAdmin::Auth::UPDATE, resource)
-            links << link_to(I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link")
+            links << link_to(I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link c-button c-button--info u-xsmall")
           end
           if controller.action_methods.include?('destroy') && authorized?(ActiveAdmin::Auth::DESTROY, resource)
-            links << link_to(I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :data => {:confirm => I18n.t('active_admin.delete_confirmation')}, :class => "member_link delete_link")
+            links << link_to(I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :data => {:confirm => I18n.t('active_admin.delete_confirmation')}, :class => "member_link delete_link c-button c-button--error u-xsmall")
           end
         end
         links << instance_exec(resource, &@other_actions) if @other_actions
